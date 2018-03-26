3894b0262b46b1737c0f8d3baeb95f92f24c17d8
andrew wong <andrew@andrews-MacBook-Pro.local>
2018-03-26 16:57:09 -0400

    refactored

--- browser/react/components/AllAlbums.js
+++ browser/react/components/AllAlbums.js
@@ -1,13 +1,99 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+
+const fakeAlbums = [
+  {
+    name: 'Abbey Road',
+    id: 1,
+    imageUrl: 'http://fillmurray.com/300/300',
+    songs: [
+      {
+        id: 1,
+        name: 'Romeo & Juliette',
+        artists: [
+          { name: 'Bill' }
+        ],
+        genre: 'Funk',
+        audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
+      },
+      {
+        id: 2,
+        name: 'White Rabbit',
+        artists: [
+          { name: 'Bill' },
+          { name: 'Alice' }
+        ],
+        genre: 'Fantasy',
+        audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
+      },
+      {
+        id: 3,
+        name: 'Lucy in the Sky with Diamonds',
+        artists: [
+          { name: 'Bob' }
+        ],
+        genre: 'Space',
+        audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
+      }
+    ]
+  },
+  {
+    name: 'Yellow Submarine',
+    id: 2,
+    imageUrl: 'http://fillmurray.com/300/300',
+    songs: [
+      {
+        id: 4,
+        name: 'London Calling',
+        artists: [
+          { name: 'Bill' }
+        ],
+        genre: 'Punk',
+        audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
+      }
+    ]
+  }
+];
 
 export default class AllAlbums extends Component {
 
+  constructor (props) {
+    super(props);
+    this.state = {
+      albums: []
+    };
+    this.selectAlbum = this.selectAlbum.bind(this);
+    this.deselectAlbum = this.deselectAlbum.bind(this);
+  }
+
+  componentDidMount () {
+    axios.get('/api/albums/')
+      .then(res => res.data)
+      .then(albums => {
+        this.setState({ albums })
+      });
+  }
+
+    selectAlbum (albumId) {
+    axios.get(`/api/albums/${albumId}`)
+      .then(res => res.data)
+      .then(album => this.setState({
+        selectedAlbum: album
+      }));
+  }
+
+  deselectAlbum () {
+    this.setState({ selectedAlbum: {}});
+  }
+
+
   render () {
-    const albums = this.props.albums;
-    const selectAlbum = this.props.selectAlbum;
+    console.log('this.props: ', this.props);
+    const albums = this.state.albums; //fakeAlbums
+    const selectAlbum = this.selectAlbum;
 
     return (
-      <div>
+      <div className="col-xs-10">
         <h3>Albums</h3>
         <div className="row">
         {
--- browser/react/components/SingleAlbum.js
+++ browser/react/components/SingleAlbum.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
 import Songs from '../components/Songs';
+import axios from 'axios';
+
+const fakeAlbum = {
+  name: 'Yellow Submarine',
+  id: 2,
+  imageUrl: 'http://fillmurray.com/300/300',
+  songs: [
+    {
+      id: 4,
+      name: 'London Calling',
+      artists: [
+        { name: 'Bill' }
+      ],
+      genre: 'Punk',
+      audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
+    }
+  ]
+};
 
 export default class SingleAlbum extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      selectedAlbum: {}
+    };
+    this.selectAlbum = this.selectAlbum.bind(this);
+    this.deselectAlbum = this.deselectAlbum.bind(this);
+  }
+
+  componentDidMount () {
+    axios.get(`/api/albums/${this.props.match.params.albumId}`)
+      .then(res => res.data)
+      .then(selectedAlbum => {
+        this.setState({ selectedAlbum })
+      });
+  }
+
+  selectAlbum (albumId) {
+    axios.get(`/api/albums/${albumId}`)
+      .then(res => res.data)
+      .then(album => this.setState({
+        selectedAlbum: album
+      }));
+  }
+
+  deselectAlbum () {
+    this.setState({ selectedAlbum: {}});
+  }
+
 
   render () {
-
-    const album = this.props.album;
+    console.log(this.props);
+    const album = this.state.selectedAlbum;
 
     return (
       <div className="album">
